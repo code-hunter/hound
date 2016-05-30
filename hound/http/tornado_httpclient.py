961aa7f83b3bf47864398d1ba9cc9829c8e8a123
code-hunter <dylanzhou0@gmail.com>
2016-05-30 16:52:49 +0800

    delete other requests

--- hound/http/tornado_httpclient.py
+++
@@ -1,9 +0,0 @@
-from tornado.curl_httpclient import CurlAsyncHTTPClient
-from tornado.httpclient import AsyncHTTPClient
-from tornado.ioloop import IOLoop
-
-class HttpClient(object):
-
-    def __init__(self):
-        self.io_loop = IOLoop()
-        self.http_client = AsyncHTTPClient(CurlAsyncHTTPClient)
